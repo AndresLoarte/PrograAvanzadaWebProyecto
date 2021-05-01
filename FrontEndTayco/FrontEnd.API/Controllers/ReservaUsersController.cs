270134fada9123bd5729ab04404c291ba03bf23e
KeLeonRU <kevinleonuma@gmail.com>
2021-05-01 01:36:03 -0600

    DNsand

---
+++ FrontEndTayco/FrontEnd.API/Controllers/ReservaUsersController.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using data = FrontEnd.API.Models;
+
+namespace FrontEnd.API.Controllers
+{
+    public class ReservaUsersController : Controller
+    {
+        string baseurl = "http://localhost:30091/";
+        public async Task<IActionResult> IndexUser()
+        {
+            List<data.Reserva> aux = new List<data.Reserva>();
+            using (var cl = new HttpClient())
+
+                //var usuario = from x in _context.Users
+                //              where x.UserId == 1
+                //              select new { x.UserId, x.UserName };
+
+            {
+                cl.BaseAddress = new Uri(baseurl);
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await cl.GetAsync("api/Reserva");
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<List<data.Reserva>>(auxres);
+                }
+            }
+            return View(aux);
+        }
+        // GET: Reserva/Details/5
+        public async Task<IActionResult> DetailsUser(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserva = GetById(id);
+
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserva);
+        }
+
+        // GET: Reserva/Create
+        public IActionResult CreateUser()
+        {
+            ViewData["UserId"] = new SelectList(GetAllUsers(), "UserId", "Correo");
+            ViewData["TipoCanchaId"] = new SelectList(GetAllTipoCancha(), "TipoCanchaId", "NombreCancha");
+            ViewData["EquipoId"] = new SelectList(GetAllEquipo(), "EquipoId", "NombreEquipo");
+            return View();
+        }
+
+        // POST: Reserva/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateUser([Bind("ReservaId,UserId,TipoCanchaId,EquipoId,FechaReserva,Hora,Estado")] Models.Reserva reserva)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var cl = new HttpClient())
+                {
+                    cl.BaseAddress = new Uri(baseurl);
+                    var content = JsonConvert.SerializeObject(reserva);
+                    var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                    var byteContent = new ByteArrayContent(buffer);
+                    byteContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                    var postTask = cl.PostAsync("api/Reserva", byteContent).Result;
+
+                    if (postTask.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+
+            return View(reserva);
+        }
+
+        //GET: Reserva/Edit/5
+        public async Task<IActionResult> EditUser(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserva = GetById(id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserva);
+        }
+        // POST: Reserva/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditUser(int id, [Bind("ReservaId,UserId,TipoCanchaId,EquipoId,FechaReserva,Hora,Estado")] Models.Reserva reserva)
+        {
+            if (id != reserva.ReservaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (var cl = new HttpClient())
+                    {
+                        cl.BaseAddress = new Uri(baseurl);
+                        var content = JsonConvert.SerializeObject(reserva);
+                        var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                        var byteContent = new ByteArrayContent(buffer);
+                        byteContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                        var postTask = cl.PutAsync("api/Reserva/" + id, byteContent).Result;
+
+                        if (postTask.IsSuccessStatusCode)
+                        {
+                            return RedirectToAction("Index");
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    var aux2 = GetById(id);
+                    if (aux2 == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(reserva);
+        }
+
+        // GET: Reserva/Delete/5
+        public async Task<IActionResult> DeleteUser(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserva = GetById(id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserva);
+        }
+
+        // POST: Reserva/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await cl.DeleteAsync("api/Reserva/" + id);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReservaExists(int id)
+        {
+            return (GetById(id) != null);
+        }
+
+        private data.Reserva GetById(int? id)
+        {
+            data.Reserva aux = new data.Reserva();
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //HttpResponseMessage res = await cl.GetAsync("api/Pais/5?"+id);
+                HttpResponseMessage res = cl.GetAsync("api/Reserva/" + id).Result;
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<data.Reserva>(auxres);
+                }
+            }
+            return aux;
+        }
+        private List<data.User> GetAllUsers()
+        {
+            List<data.User> aux = new List<data.User>();
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //HttpResponseMessage res = await cl.GetAsync("api/Pais/5?"+id);
+                HttpResponseMessage res = cl.GetAsync("api/Users").Result;
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<List<data.User>>(auxres);
+                }
+            }
+            return aux;
+        }
+
+        private List<data.TipoCancha> GetAllTipoCancha()
+        {
+            List<data.TipoCancha> aux = new List<data.TipoCancha>();
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //HttpResponseMessage res = await cl.GetAsync("api/Pais/5?"+id);
+                HttpResponseMessage res = cl.GetAsync("api/TipoCancha").Result;
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<List<data.TipoCancha>>(auxres);
+                }
+            }
+            return aux;
+        }
+
+        private List<data.Equipo> GetAllEquipo()
+        {
+            List<data.Equipo> aux = new List<data.Equipo>();
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //HttpResponseMessage res = await cl.GetAsync("api/Pais/5?"+id);
+                HttpResponseMessage res = cl.GetAsync("api/Equipo").Result;
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<List<data.Equipo>>(auxres);
+                }
+            }
+            return aux;
+        }
+
+        //private readonly data.TaycoContext _context;
+
+        //public ReservaUsersController(data.TaycoContext context) {
+
+        //    _context = context;
+        //}
+
+    }
+}
---
+++ FrontEndTayco/FrontEnd.API/Controllers/UserController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using data = FrontEnd.API.Models;
+
+namespace FrontEnd.API.Controllers
+{
+    public class UserController : Controller
+    {
+        string baseurl = "http://localhost:30091/";
+
+        public async Task<IActionResult> Index()
+        {
+            List<data.User> aux = new List<data.User>();
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await cl.GetAsync("api/Users");
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<List<data.User>>(auxres);
+                }
+            }
+            return View(aux);
+        }
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            id = 1;
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var users = GetById(id);
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return View(users);
+        }
+
+
+        //GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            id = 1;
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var users = GetById(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return View(users);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,UserName,Nombre,PrimerApellido,SegundoApellido,Correo,Telefono,Password,Estado,RoleId")] data.User users)
+        {
+            id = 1;
+            if (id != users.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (var cl = new HttpClient())
+                    {
+                        cl.BaseAddress = new Uri(baseurl);
+                        var content = JsonConvert.SerializeObject(users);
+                        var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                        var byteContent = new ByteArrayContent(buffer);
+                        byteContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                        var postTask = cl.PutAsync("api/Users/" + id, byteContent).Result;
+
+                        if (postTask.IsSuccessStatusCode)
+                        {
+                            return RedirectToAction("Index");
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    var aux2 = GetById(id);
+                    if (aux2 == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(users);
+        }
+
+
+        private bool UsersExists(int id)
+        {
+            return (GetById(id) != null);
+        }
+
+        private data.User GetById(int? id)
+        {
+            data.User aux = new data.User();
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //HttpResponseMessage res = await cl.GetAsync("api/Pais/5?"+id);
+                HttpResponseMessage res = cl.GetAsync("api/Users/" + id).Result;
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<data.User>(auxres);
+                }
+            }
+            return aux;
+        }
+
+        private List<data.Roles> GetAllRoles()
+        {
+            List<data.Roles> aux = new List<data.Roles>();
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //HttpResponseMessage res = await cl.GetAsync("api/Pais/5?"+id);
+                HttpResponseMessage res = cl.GetAsync("api/Roles").Result;
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<List<data.Roles>>(auxres);
+                }
+            }
+            return aux;
+        }
+
+    }
+}
